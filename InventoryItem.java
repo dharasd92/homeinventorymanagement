70031f2910e4883f93c2781a5cd20aa0ee20f1f8
sourav dhara <39241810+dharasd92@users.noreply.github.com>
2021-03-25 19:14:07 +0530

    Add files via upload

---
+++ InventoryItem.java
@@ -0,0 +1,13 @@
+package old;
+class InventoryItem{
+	public String location;
+	public String serialNumber;
+	public boolean marked;
+	public String purchasePrice;
+	public String purchaseDate;
+	public String purchaseLocation;
+	public String note;
+	public String photoFile;
+	public String description;
+}
+
